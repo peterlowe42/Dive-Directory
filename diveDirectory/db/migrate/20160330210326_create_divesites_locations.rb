ab02122370f52a50b9f40681d7117ed0950c43cc
peterlowe42 <peter.lowe42@gmail.com>
2016-03-30 14:11:53 -0700

    Add migration for divesite location join table

--- diveDirectory/db/migrate/20160330210326_create_divesites_locations.rb
+++ diveDirectory/db/migrate/20160330210326_create_divesites_locations.rb
@@ -1,6 +1,8 @@
 class CreateDivesitesLocations < ActiveRecord::Migration
   def change
     create_table :divesites_locations do |t|
+      t.references :dive_site
+      t.references :location
     end
   end
 end
