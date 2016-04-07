523c53d85cae98342c6b72e72a1c86663c650b9b
peterlowe42 <peter.lowe42@gmail.com>
2016-04-07 14:32:31 -0700

    Add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
   end
  
   def create
+    @user = User.new(user_params)
+    if @user.save
+      @message = "Congratulations You've registered"
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation, :first_name, :last_name)
   end
 end
