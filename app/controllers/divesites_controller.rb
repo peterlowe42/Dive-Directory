a9b5eb27e0ee831aa35382d33522814eee9fa053
Pete <Peter Lowe>
2016-05-05 20:52:28 -0700

    Make flash messages pretty and chenge px values to em

--- app/controllers/divesites_controller.rb
+++ app/controllers/divesites_controller.rb
@@ -10,16 +10,15 @@ class DivesitesController < ApplicationController
         resp = HTTParty.get(generate_url(coords[0],coords[1])).to_json
         data_hash = JSON.parse(resp)
         site_data = data_hash['sites']
-        @lat = data_hash['request']['loc']['lat']
-        @lng = data_hash['request']['loc']['lng']
         @location = check_and_update_db(site_data, location, @lat, @lng)
-        @hash = Gmaps4rails.build_markers(@location.divesites) do |site, marker|
+        @divesites = @location.divesites
+        @hash = Gmaps4rails.build_markers(@divesites) do |site, marker|
           marker.lat site.lat
           marker.lng site.lng
           marker.infowindow site.site_name
         end
       else
-        flash[:danger] = "Sorry we could not find that location"
+        flash[:error] = "Sorry we could not find that location"
       end
     end
   end
