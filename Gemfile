8a14f61d75b5e587d0ec8c1a34477f3f1d9cae1e
Pete <Peter Lowe>
2016-05-15 15:08:53 -0700

    Add puma to gem file

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'gmaps4rails'
 
 gem 'sidekiq'
 
+gem 'puma'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/divsites.js
+++ app/assets/javascripts/divsites.js
@@ -4,17 +4,14 @@ $( document ).ready(function() {
     	$('#welcome').hide();
     	$('#loading-message').show();
     	var url = $(this).attr('action');
-    	console.log(url);
     	var data = $(this).serialize();
-    	console.log(data);
     	$.ajax({
     		url: url,
     		method: 'get',
     		data: data
     	}).done(function(response){
-            $("#loading-message").hide()
-            console.log(response)
-            $("#content").html(response)
+            $("#loading-message").hide();
+            $("#content").html(response);
         })
     });
 });
